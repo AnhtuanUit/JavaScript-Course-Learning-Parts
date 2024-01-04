8da863be188e4a86ebf0a5486697b93ce2de5ba4
TuanDo <anhtuan7194@gmail.com>
2024-01-04 18:10:47 +0700

    Values and Variables

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,3 +1,27 @@
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN");
+// if (js === "amazing") alert("JavaScript is FUN");
 console.log(40 + 8 + 23 - 10);
+
+// console.log("Jonas");
+// console.log(23);
+
+/* 1) VALUES AND VARIABLES */
+let firstName = "Tuan";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let do_anh_tuan = "DAT";
+let $function = 27;
+
+let person = "tuando";
+let PI = 3.1415;
+
+let myFirstJob = "Programer";
+let mySecondJob = "Youtuber";
+
+let job1 = "programer";
+let job2 = "youtuber";
+
+console.log(myFirstJob);
