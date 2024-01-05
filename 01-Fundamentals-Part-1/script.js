2a478931a363ed792d1b7f589dba17704acc5638
TuanDo <anhtuan7194@gmail.com>
2024-01-05 08:31:51 +0700

    Data Types

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,3 +1,4 @@
+/*
 let js = "amazing";
 // if (js === "amazing") alert("JavaScript is FUN");
 console.log(40 + 8 + 23 - 10);
@@ -5,13 +6,13 @@ console.log(40 + 8 + 23 - 10);
 // console.log("Jonas");
 // console.log(23);
 
-/* 1) VALUES AND VARIABLES */
+// 1) VALUES AND VARIABLES
 let firstName = "Tuan";
 
 console.log(firstName);
 console.log(firstName);
 console.log(firstName);
-
+// Variable name convention
 let do_anh_tuan = "DAT";
 let $function = 27;
 
@@ -25,3 +26,24 @@ let job1 = "programer";
 let job2 = "youtuber";
 
 console.log(myFirstJob);
+
+*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(23);
+// console.log("TuanDo");
+console.log(typeof javascriptIsFun);
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1994;
+console.log(typeof year);
+
+console.log(typeof null);
