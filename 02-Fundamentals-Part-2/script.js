322714532cbb480b0e692fa836d91dc29c501679
TuanDo <anhtuan7194@gmail.com>
2024-01-06 15:06:05 +0700

    Function Declarations vs. Expression

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -1,3 +1,6 @@
+"use strict";
+/*
+// Functions
 function logger() {
   console.log("My name is Tuan");
 }
@@ -19,3 +22,22 @@ const appleOrangeJuice = fruitProgress(3, 4);
 console.log(appleOrangeJuice);
 
 const num = Number(23);
+*/
+
+const hoistingAge1 = calcAge1(1994);
+
+// Function declaration
+function calcAge1(birthYear) {
+  return 2037 - birthYear;
+}
+
+const age1 = calcAge1(1994);
+
+// Function expresstion
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const age2 = calcAge2(1994);
+
+console.log(age1, age2, hoistingAge1);
