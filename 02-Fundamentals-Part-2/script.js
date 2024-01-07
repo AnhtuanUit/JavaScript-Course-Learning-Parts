03b3409f5ef4234b81c5a8b8b965eb21093a0a3b
TuanDo <anhtuan7194@gmail.com>
2024-01-07 09:56:45 +0700

    CHALLENGE #2 cover: The Conditional(Ternary) Operator, Functions, Arrow Functions

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -203,7 +203,7 @@ const ages = [
   calcAge(years2[years2.length - 1]),
 ];
 */
-
+/*
 // 40. Basic Array Operations(Methods)
 const friends = ["Phuoc", "Loc", "Tung"];
 
@@ -234,3 +234,35 @@ console.log(friends.includes("Bob"));
 if (friends.includes("Phuoc")) {
   console.log("You have a friend called Phuoc");
 }
+
+*/
+
+/*
+
+CHALLENGE #2
+Steven wants you to improve his tip calculator, using the same rules as before — tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+
+Your tasks:
+
+Write a function calcTip that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from the first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100.
+
+And now let's use arrays! So, create an array called bills containing the test data below.
+
+Create an array called tips containing the tip value for each bill, calculated from the function you created before.
+
+BONUS: Create an array totals containing the total values, so the bill + tip.
+
+TEST DATA: 125, 555, and 44.
+
+*/
+/* Write your code below. Good luck! 🙂 */
+
+const calcTip = (bill) =>
+  50 <= bill && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+console.log(calcTip(100));
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const totals = [tips[0] + bills[0], tips[1] + bills[1], tips[2] + bills[2]];
+console.log(tips, totals);
