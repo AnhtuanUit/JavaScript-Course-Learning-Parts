56683d3f0860f6dd8cc55ed77fb62bfa3df421b4
TuanDo <anhtuan7194@gmail.com>
2024-01-08 20:46:54 +0700

    Handle Click Events

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -1,12 +1,9 @@
 'use strict';
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = '👋  Correct number';
 
-console.log(document.querySelector('.number').textContent);
-document.querySelector('.number').textContent = 23;
-
-console.log(document.querySelector('.score').textContent);
-document.querySelector('.score').textContent = 10;
-
-document.querySelector('.guess').value = 10;
-console.log(document.querySelector('.guess').value);
+document.querySelector('.check').addEventListener('click', function (e) {
+  e.preventDefault();
+  const guess = document.querySelector('.guess').value;
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔️ No number!';
+  }
+});
