c3b2475fed94154fb9f330c102f655740c997e88
TuanDo <anhtuan7194@gmail.com>
2024-01-08 20:16:27 +0700

    Selecting and Manipulating Elements

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -1,3 +1,12 @@
 'use strict';
-const statusMessage = document.querySelector('.message');
-console.log(statusMessage);
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '👋  Correct number';
+
+console.log(document.querySelector('.number').textContent);
+document.querySelector('.number').textContent = 23;
+
+console.log(document.querySelector('.score').textContent);
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 10;
+console.log(document.querySelector('.guess').value);
