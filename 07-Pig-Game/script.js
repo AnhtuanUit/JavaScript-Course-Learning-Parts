e2b7a5d9f255742b9caf2f858f51ec13b286d40a
TuanDo <anhtuan7194@gmail.com>
2024-01-09 10:50:58 +0700

    Switching the Active Player

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
 const diceEl = document.querySelector('.dice');
@@ -15,8 +17,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
-let currentScore0 = 0;
+let currentScore = 0;
 let totalScore0 = 0;
+let activePlayer = 0;
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -26,19 +29,23 @@ btnRoll.addEventListener('click', function () {
   if (diceEl.classList.contains('hidden')) diceEl.classList.remove('hidden');
   diceEl.src = `dice-${dice}.png`;
   // 3. Check if dice roll = 1
-  if (dice === 1) {
-    // 3.1 Switch to next player
-    curScore0El.textContent = 0;
+  if (dice !== 1) {
+    // Add dice roll to current score
+    currentScore += dice;
+
+    // Display new score
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+
+    // Check if score >= 100 -> Diplay current user win
+    // if (totalScore0 + currentScore > 100) {
+    //   alert('Player 1 win');
+    // }
   } else {
-    // 3.2 Add dice roll to current score
-    currentScore0 += dice;
-
-    // 4. Display new score
-    curScore0El.textContent = currentScore0;
-
-    // 5. Check if score >= 100 -> Diplay current user win
-    if (totalScore0 + currentScore0 > 100) {
-      alert('Player 1 win');
-    }
+    // Switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
