7d50e8caf46eca095ecfbb178cb046bf64efa819
TuanDo <anhtuan7194@gmail.com>
2024-01-09 09:08:15 +0700

    Working with Classes

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -3,9 +3,19 @@
 const model = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseMoal = document.querySelector('.close-modal');
-
 const btnsOpenMoal = document.querySelectorAll('.show-modal');
 
-for (let i = 0; i < btnsOpenMoal.length; i++) {
-  console.log(btnsOpenMoal[i].textContent);
-}
+const openModal = function () {
+  model.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  model.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenMoal.length; i++)
+  btnsOpenMoal[i].addEventListener('click', openModal);
+btnCloseMoal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
