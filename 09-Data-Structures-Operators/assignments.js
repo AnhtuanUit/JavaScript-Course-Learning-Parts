9a152e4349555b0df9e9c010b3b22e92552d7e12
TuanDo <anhtuan7194@gmail.com>
2024-01-11 15:26:32 +0700

    Coding Challenge #3

--- 09-Data-Structures-Operators/assignments.js
+++ 09-Data-Structures-Operators/assignments.js
@@ -557,13 +557,13 @@ if (bookMap.has('author')) {
 // Convert the first book object from the books array into a Map, and assign it to a firstBookMap variable.
 
 const firstBookMap = new Map(Object.entries(books[0]));
-console.log(firstBookMap);
+// console.log(firstBookMap);
 
 // 14.2
 // Use the for-of loop to iterate over firstBookMap, and log to the console keys that have numbers as values.
 
 for (const [key, value] of firstBookMap) {
   if (typeof value === 'number') {
-    console.log(key);
+    // console.log(key);
   }
 }
