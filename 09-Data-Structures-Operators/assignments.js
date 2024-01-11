4aefc0bc00934fb936969754ce11a1e83d51e72b
TuanDo <anhtuan7194@gmail.com>
2024-01-11 11:43:31 +0700

    ASSIGNMENT: Map Fundamentals

--- 09-Data-Structures-Operators/assignments.js
+++ 09-Data-Structures-Operators/assignments.js
@@ -492,30 +492,62 @@ const allKeywords = [];
 for (const { keywords } of books) {
   allKeywords.push(...keywords);
 }
-console.log(allKeywords);
+// console.log(allKeywords);
 
 // 12.2
 // The allKeyword array contains duplicates. Remove them by creating a Set out of that array. Assign the newly created set to the uniqueKeywords variable.
 const uniqueKeywords = new Set(allKeywords);
-console.log(uniqueKeywords);
+// console.log(uniqueKeywords);
 
 // 12.3
 // Add two more keywords to the uniqueKeywords set, for example, 'coding' and 'science'.
 uniqueKeywords.add('coding');
 uniqueKeywords.add('science');
-console.log(uniqueKeywords);
+// console.log(uniqueKeywords);
 
 // 12.4
 // Delete 'business' from the uniqueKeywords set.
 uniqueKeywords.delete('business');
-console.log(uniqueKeywords);
+// console.log(uniqueKeywords);
 
 // 12.5
 // Create an array out of the uniqueKeywords set, and assign it to the uniqueKeywordsArr variable.
 const uniqueKeywordsArr = [...uniqueKeywords];
-console.log(uniqueKeywordsArr);
+// console.log(uniqueKeywordsArr);
 
 // 12.6
 // Delete all items from the uniqueKeywords set.
 uniqueKeywords.clear();
-console.log(uniqueKeywords);
+// console.log(uniqueKeywords);
+
+////////////////////////////////////////////////
+// Maps: Fundamentals
+// 13.1
+// Create a new book, but this time, as a Map. Assign it to the bookMap variable. Use this array as initial data:
+[
+  ['title', 'Clean Code'],
+  ['author', 'Robert C. Martin'],
+];
+const bookMap = new Map([
+  ['title', 'Clean Code'],
+  ['author', 'Robert C. Martin'],
+]);
+console.log(bookMap);
+// 13.2
+// Set a new key in bookMap called pages, and assign it with a number 464.
+bookMap.set('pages', 464);
+console.log(bookMap);
+
+// 13.3
+// Get the title and author values from bookMap, and log to the console a string formatted like that: "${title} by ${author}".
+console.log(`${bookMap.get('title')} by ${bookMap.get('author')}`);
+
+// 13.4
+// Get the size of bookMap, and log it to the console.
+console.log(bookMap.size);
+
+// 13.5
+// Check if bookMap has the author key. and if so, log "The author of the book is known" to the console.
+if (bookMap.has('author')) {
+  console.log('The author of the book is known');
+}
