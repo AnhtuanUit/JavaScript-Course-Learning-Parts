b853b72539a0520fc24ed05060748d5d19a8bf72
TuanDo <anhtuan7194@gmail.com>
2024-01-11 09:33:03 +0700

    ASSIGNMENT: Enhanced Object Literals

--- 09-Data-Structures-Operators/assignments.js
+++ 09-Data-Structures-Operators/assignments.js
@@ -375,7 +375,7 @@ for (let i = 0; i < books.length; i++) {
 
 let pageSum = 0;
 for (const book of books) pageSum += book.pages;
-console.log(pageSum);
+// console.log(pageSum);
 
 // 8.2
 // Below is the allAuthors variable which stores an empty array. Use the for-of loop to fill allAuthors with the authors of each book from the books array.
@@ -389,10 +389,41 @@ for (const { author = [] } of books) {
     for (const el of author) allAuthors.push(el);
   }
 }
-console.log(allAuthors);
+// console.log(allAuthors);
 
 // 8.3
 // Use the for-of loop together with Array's entries() method to log each author from allAuthors to the console together with its index. Make the index start from 1, instead of 0.
 for (const [i, author] of allAuthors.entries()) {
-  console.log(`${i + 1}. ${author}`);
+  // console.log(`${i + 1}. ${author}`);
 }
+
+////////////////////////////////////////////////
+// Enhanced Object Literals
+// 9.1
+// Below is the bookData array that contains other arrays. Each inner array consists of the property name (first element), and the value (second element). For example, in ['title', 'Computer Networking: A Top-Down Approach'], 'title' is the property name, and 'Computer Networking: A Top-Down Approach' is meant to be the value assigned to that property name.
+
+// Using computed properties, fill the newBook object with the properties and values from the bookData array. The first one is done already.
+const bookData = [
+  ['title', 'Computer Networking: A Top-Down Approach'],
+  ['author', ['James F. Kurose', 'Keith W. Ross']],
+  ['publisher', 'Addison Wesley'],
+];
+
+// Do the rest
+const newBook = {
+  [bookData[0][0]]: bookData[0][1],
+  [bookData[1][0]]: bookData[1][1],
+  [bookData[2][0]]: bookData[2][1],
+};
+console.log(newBook);
+
+// 9.2
+// Below is the pages variable. Add it as a property of the newBook2 object. Use the shorter way.
+const pages = 880;
+
+const newBook2 = {
+  title: 'The C Programming Language',
+  author: ['Brian W. Kernighan', 'Dennis M. Ritchie'],
+  // ...
+  pages,
+};
