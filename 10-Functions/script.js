48a89f3d10518ccd2d4b09af3f512be84b5cf781
TuanDo <anhtuan7194@gmail.com>
2024-01-12 16:00:35 +0700

    Default Parameters

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,1 +1,29 @@
 'use strict';
+////////////////////////////////////////////////
+// Default Parameters
+
+const bookings = [];
+
+const createBooking = function (
+  flightNumber,
+  numPassengers = 1,
+  price = numPassengers * 199
+) {
+  // ES5
+  // numPassengers = numPassengers || 1;
+  // price = price || 199 * numPassengers;
+
+  const booking = {
+    flightNumber,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 2);
+createBooking('LH123', 5);
+createBooking('LH123', undefined, 1000);
