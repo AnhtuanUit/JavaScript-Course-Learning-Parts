a6fbe8bee0cda86aa0ca657d69324a0167198b26
TuanDo <anhtuan7194@gmail.com>
2024-01-12 16:12:09 +0700

    How Passing Arguments Works: Values vs. Reference

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -2,7 +2,7 @@
 ////////////////////////////////////////////////
 // Default Parameters
 
-const bookings = [];
+/* const bookings = [];
 
 const createBooking = function (
   flightNumber,
@@ -26,4 +26,39 @@ createBooking('LH123');
 createBooking('LH123', 2, 800);
 createBooking('LH123', 2);
 createBooking('LH123', 5);
-createBooking('LH123', undefined, 1000);
+createBooking('LH123', undefined, 1000); */
+////////////////////////////////////////////////
+// How Passing Arguments Works: Values vs. Reference
+// 1) Primitives Values
+const flight = 'LH234';
+// 2) Object(Reference Values)
+const tuan = {
+  name: 'Tuan Do',
+  passport: 253677818,
+};
+
+const checkIn = function (flightNumber, passenger) {
+  flightNumber = 'LH999';
+  passenger.name = 'Mr.' + passenger.name;
+
+  if (passenger.passport === 253677818) {
+    console.log('Checked in');
+  } else {
+    console.log('Wrong passport');
+  }
+};
+
+checkIn(flight, tuan);
+console.log(flight);
+console.log(tuan);
+
+// Is the same as doing...
+// const flighNum = flight;
+// const passenger = tuan;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000);
+};
+
+newPassport(tuan);
+checkIn(flight, tuan);
