77c61d5f0a418930b9d97e9c1f28b513c96f031a
TuanDo <anhtuan7194@gmail.com>
2024-01-13 08:39:44 +0700

    More Closure Examples

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -307,7 +307,7 @@ console.log(notPrivate);
  */
 ////////////////////////////////////////////////
 // Closures
-const securityBooking = function () {
+/* const securityBooking = function () {
   let passengerCount = 0;
   return function () {
     passengerCount++;
@@ -319,4 +319,44 @@ const booker = securityBooking();
 booker();
 booker();
 booker();
-console.dir(booker);
+console.dir(booker); */
+
+////////////////////////////////////////////////
+// More Closure Examples
+// Example 1
+let f;
+const g = function () {
+  const a = 10;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 25;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+console.dir(f);
+
+// Re-assigning f function
+h();
+f();
+console.dir(f);
+
+// Example 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 group, each with ${perGroup} passenger`);
+  }, wait * 1000);
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+const perGroup = 1000;
+boardPassengers(9, 1);
