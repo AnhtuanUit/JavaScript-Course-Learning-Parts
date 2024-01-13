27be77ed458c748ade7f69a018412368438f08b0
TuanDo <anhtuan7194@gmail.com>
2024-01-13 08:59:22 +0700

    Coding Challeng #2: Closer Look at Functions

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -324,7 +324,7 @@ console.dir(booker); */
 ////////////////////////////////////////////////
 // More Closure Examples
 // Example 1
-let f;
+/* let f;
 const g = function () {
   const a = 10;
   f = function () {
@@ -359,4 +359,25 @@ const boardPassengers = function (n, wait) {
 };
 
 const perGroup = 1000;
-boardPassengers(9, 1);
+boardPassengers(9, 1); */
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+This is more of a thinking challenge than a coding challenge 🤓
+
+Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
+
+And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need. Think about WHEN exactly the callback function is executed, and what that means for the variables involved in this example.
+
+GOOD LUCK 😀
+*/
+(() => {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+
+  document.body.addEventListener('click', () => {
+    header.style.color = 'blue';
+  });
+})();
