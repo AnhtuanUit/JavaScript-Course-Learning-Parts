382c2fbfeff4eb231e64090ecee930c922c2a9a8
TuanDo <anhtuan7194@gmail.com>
2024-01-14 09:44:03 +0700

    More Ways of Creating and filling Arrays

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -541,7 +541,7 @@ console.log(overalBalance2);
 // Sorting Arrays
 
 // Strings
-const owners = ['Jonas', 'Zach', 'Adam', 'Martha'];
+/* const owners = ['Jonas', 'Zach', 'Adam', 'Martha'];
 console.log(owners.sort());
 console.log(owners);
 
@@ -566,3 +566,39 @@ console.log(movements);
 // });
 movements.sort((a, b) => b - a);
 console.log(movements);
+ */
+////////////////////////////////////////////////
+// More Ways of Creating and filling Arrays
+const arr = [1, 2, 3, 4, 5, 6, 7];
+console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+
+// Empty arrays + fill method
+const x = new Array(7);
+console.log(x);
+
+// Can NOT fill by .map
+// console.log(x.map(() => 5));
+x.fill(1, 3, 5); // [emptyx3, 1, 1, emptyx2];
+console.log(x);
+x.fill(1); // [1, 1, 1, 1, 1, 1, 1];
+console.log(x);
+
+// Array.from
+console.log('---ARRAY.FROM---');
+const y = Array.from({ length: 7 }, () => 1);
+console.log(y); // [1, 1, 1, 1, 1, 1, 1]
+
+const z = Array.from({ length: 7 }, (_, i) => i + 1);
+console.log(z); // [1, 2, 3, 4, 5, 6, 7]
+
+labelBalance.addEventListener('click', () => {
+  const movementsUI = Array.from(
+    document.querySelectorAll('.movements__value'),
+    el => Number(el.textContent.replace('€', ''))
+  );
+  console.log(movementsUI);
+  const movementUI2 = [...document.querySelectorAll('.movements__value')].map(
+    el => Number(el.textContent.replace('€', ''))
+  );
+  console.log(movementUI2);
+});
