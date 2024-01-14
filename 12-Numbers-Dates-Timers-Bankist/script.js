b5690c52f372d3089e5f29c9c6118ef7b5319ff6
TuanDo <anhtuan7194@gmail.com>
2024-01-14 11:12:18 +0700

    Converting and Checking Numbers

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -251,3 +251,44 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+// Converting and Checking Numbers
+console.log(23 === 23.0);
+
+// Base 10 - 0  to 9. 1/10 = 0.1. 3/10 = 3.333333333
+console.log('---JAVASCRIPT_BASE_BINARY---');
+console.log(0.1 + 0.2);
+console.log(0.1 + 0.2 === 0.3);
+
+// Conversion
+
+console.log('---CONVERSION---');
+console.log(Number('23'));
+console.log(+'23');
+
+// Parsing
+console.log('---PARSING---');
+console.log(Number.parseInt('30px', 10));
+console.log(Number.parseInt('e23'), 10);
+
+console.log(Number.parseInt('    2.5rem    '));
+console.log(Number.parseFloat('    2.5rem   '));
+
+// console.log(parseFloat('    2.5rem   '));
+
+// Check if value is NaN
+console.log('---CHEKC_VALUE_NaN---');
+console.log(Number.isNaN(20)); // false
+console.log(Number.isNaN('20')); // false
+console.log(Number.isNaN(+'20X')); // true
+console.log(Number.isNaN(23 / 0)); // false
+
+// Check if value is number
+console.log('---CHECK_VALUE_NUMBER---');
+console.log(Number.isFinite(20)); // true
+console.log(Number.isFinite('20')); // false
+console.log(Number.isFinite(+'20X')); // false
+console.log(Number.isFinite(23 / 0)); // false
+
+console.log(Number.isInteger(23)); // true
+console.log(Number.isInteger(23.0)); // true
+console.log(Number.isInteger(23 / 0)); // false
