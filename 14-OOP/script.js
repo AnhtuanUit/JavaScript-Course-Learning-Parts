d9f25a89e30db33294d753a20c08adc688071791
TuanDo <anhtuan7194@gmail.com>
2024-01-17 17:02:22 +0700

    Another Class Example

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -435,7 +435,7 @@ martha.calcAge(); */
 
 ////////////////////////////////////////////////
 // Inheritance Betwwen "Classes": Object.create
-
+/* 
 const PersonProto = {
   calcAge() {
     console.log(2027 - this.birthYear);
@@ -464,3 +464,47 @@ const jay = Object.create(StudentProto);
 jay.init('Yay', 2020, 'Computer Science');
 jay.introduce();
 jay.calcAge();
+*/
+
+////////////////////////////////////////////////
+// Another Class Example
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+    this.movements = [];
+    console.log(`Thank for openning an account, ${this.owner}`);
+  }
+
+  // Public interface
+  deposit(val) {
+    this.movements.push(val);
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+  }
+  approveLoan(val) {
+    return true;
+  }
+
+  requestLoan(val) {
+    if (this.approveLoan(val)) {
+      this.deposit(val);
+      console.log('Loan approved');
+    }
+  }
+}
+
+const acc1 = new Account('Tuan', 'VND', 1111);
+
+// acc1.movements.push(100);
+// acc1.movements.push(-400);
+acc1.deposit(100);
+acc1.withdraw(400);
+acc1.requestLoan(1000);
+acc1.approveLoan(1000);
+
+console.log(acc1);
+console.log(acc1.pin);
