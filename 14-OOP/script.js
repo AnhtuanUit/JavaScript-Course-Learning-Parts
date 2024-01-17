5010e0ab074aff0971b4690aaae9fd208336941a
TuanDo <anhtuan7194@gmail.com>
2024-01-17 14:37:50 +0700

    Object.create

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -195,3 +195,26 @@ console.log(account.movements);
 // Setter and Getters in Class: PersonCl
 const walter = new PersonCl('Walter White', 1965);
 console.log(walter.age);
+
+////////////////////////////////////////////////
+// Object.create
+
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const steven = Object.create(PersonProto);
+console.log(steven);
+steven.firstName = 'Steven';
+steven.birthYear = 2000;
+console.log(steven.__proto__ === PersonProto);
+
+const sarah = Object.create(PersonProto);
+sarah.init('Sarah', 2007);
+sarah.calcAge();
