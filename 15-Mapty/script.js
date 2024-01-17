19dce1e669bd6304204ed2dc3776a27a9d0a3134
TuanDo <anhtuan7194@gmail.com>
2024-01-17 21:27:54 +0700

    Using the Geolocation API

--- 15-Mapty/script.js
+++ 15-Mapty/script.js
@@ -10,3 +10,20 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const {
+        coords: { latitude, longitude },
+      } = position;
+      console.log(latitude, longitude);
+      console.log(
+        `https://www.google.com/maps/search/${latitude},+${longitude}?entry=tts`
+      );
+    },
+    function () {
+      alert('Could not get your position');
+    }
+  );
+}
