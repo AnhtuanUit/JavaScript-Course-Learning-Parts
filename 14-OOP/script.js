3b1438b366130aef510eea94342448c7f366ea1d
TuanDo <anhtuan7194@gmail.com>
2024-01-17 18:04:53 +0700

    Coding Challenge #4: OOP

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -569,7 +569,7 @@ console.log(acc1.pin);
 // 3) Public methods
 // 4) Private methods
 // (there is also the static version)
-class Account {
+/* class Account {
   // 1) Public field (instances)
   locale = navigator.language;
 
@@ -634,3 +634,64 @@ console.log(acc1.pin);
 // Chaining
 acc1.deposit(300).deposit(500).withdraw(35).requestLoan(25000).withdraw(4000);
 console.log(acc1.getMovements());
+ */
+
+///////////////////////////////////////
+// Coding Challenge #4
+
+/* 
+1. Re-create challenge #3, but this time using ES6 classes: create an 'EVCl' child class of the 'CarCl' class
+2. Make the 'charge' property private;
+3. Implement the ability to chain the 'accelerate' and 'chargeBattery' methods of this class, and also update the 'brake' method in the 'CarCl' class. They experiment with chining!
+
+DATA CAR 1: 'Rivian' going at 120 km/h, with a charge of 23%
+
+GOOD LUCK 😀
+*/
+// 1. 2.
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+  accelerate() {
+    this.speed += 10;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+  }
+
+  brake() {
+    this.speed = this.speed > 5 ? this.speed - 5 : 0;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+    return this;
+  }
+}
+
+class EV extends CarCl {
+  // Private fields
+  #charge;
+
+  constructor(make, speed, charge) {
+    super(make, speed);
+    this.#charge = charge;
+  }
+
+  chargeBattery(chargeTo) {
+    this.#charge = chargeTo;
+    return this;
+  }
+
+  accelerate() {
+    this.speed += 20;
+    this.#charge -= 1;
+    console.log(
+      `${this.make} is going at ${this.speed} km/h, with a charge of ${
+        this.#charge
+      }%`
+    );
+    return this;
+  }
+}
+// 4.
+const rivian = new EV('Rivian', 100, 10);
+rivian.chargeBattery(24).accelerate().brake();
+console.dir(rivian);
