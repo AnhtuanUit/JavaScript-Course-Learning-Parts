dc31d123214ff2c7e04d145f9f307440b526b3da
TuanDo <anhtuan7194@gmail.com>
2024-01-17 11:18:50 +0700

    ES6 Classes

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -125,3 +125,38 @@ mercedes.accelerate();
 mercedes.accelerate();
 mercedes.brake();
 mercedes.brake();
+
+////////////////////////////////////////////////
+// ES6 Classes
+console.log('---ES6_CLASSES---');
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+
+  // Instance methods
+  // Methods will be added to .prototype property
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+  greet() {
+    console.log(`Hey ${this.firstName}`);
+  }
+}
+
+const jessica = new PersonCl('Jessica Davis', 1996);
+console.log(jessica);
+jessica.calcAge();
+
+console.log(jessica.__proto__ === PersonCl.prototype);
+
+// PersonCl.prototype.greet = function () {
+//   console.log(`Hey ${this.firstName}`);
+// };
+
+jessica.greet();
+
+// 1. Classes are NOT hoisted
+// 2. Classes are first-class citizens
+// 3. Classes are executed in strict mode
