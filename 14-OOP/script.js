7c422b5ddc2740715190c818fd96bddb9bcaf78e
TuanDo <anhtuan7194@gmail.com>
2024-01-17 10:40:32 +0700

    Constructor Functions and the new Operator

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -1,1 +1,34 @@
 'use strict';
+////////////////////////////////////////////////
+// Constructor Functions and the new Operator
+const Person = function (firstName, birthYear) {
+  // Instant properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // Never do this!
+  // this.calcAge = function () {
+  //   console.log(11);
+  //   console.log(2037 - this.birthYear);
+  // };
+};
+
+const tuan = new Person('Tuan', 1994);
+
+console.log(tuan);
+
+// 1. New {} is created
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+// 4. function automatically return {}
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
+
+console.log(tuan instanceof Person);
+
+Person.hey = function () {
+  console.log('Hey there 👋');
+  console.log(this);
+};
+Person.hey();
