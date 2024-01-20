64f967daed48193bf47dbcfb4491291bc06c31a0
TuanDo <anhtuan7194@gmail.com>
2024-01-20 09:25:31 +0700

    CommonJS Modules

--- 17-Modern-JS-Modules-Tooling/script.js
+++ 17-Modern-JS-Modules-Tooling/script.js
@@ -50,3 +50,46 @@ const lastPost = getLastPost();
 
 const lastPost2 = await getLastPost();
 console.log(lastPost2);
+
+////////////////////////////////////////////////
+// The Module Pattern
+
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+ShoppingCart2.addToCart('apple', 4); // 4 apple ordered from supplier
+ShoppingCart2.addToCart('pizza', 2); // 2 pizza ordered from supplier
+
+console.log(ShoppingCart2);
+console.log(ShoppingCart2.shippingCost);
+
+///////////////////////////////////////
+// CommonJS Modules
+// Export
+/* export.addTocart = function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(
+    `${quantity} ${product} added to cart (sipping cost is ${shippingCost})`
+  );
+};
+
+// Import
+const { addTocart } = require('./shoppingCart.js');
+console.log(addToCart);
+ */
