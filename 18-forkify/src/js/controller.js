a90e308d967a06272211d0278883f371690ebf4b
TuanDo <anhtuan7194@gmail.com>
2024-01-20 21:55:09 +0700

    Listening For load and hashchange Events

--- 18-forkify/src/js/controller.js
+++ 18-forkify/src/js/controller.js
@@ -23,6 +23,7 @@ const renderSpinner = function () {
           </svg>
         </div>`;
 
+  recipeContainer.innerHTML = '';
   recipeContainer.insertAdjacentHTML('afterbegin', html);
 };
 
@@ -125,10 +126,15 @@ const renderHtml = function (recipe) {
 };
 
 const showRecipe = async function () {
-  renderSpinner();
+  const id = window.location.hash.slice(1);
+  console.log(id);
+  if (!id) return;
+
   try {
+    // 1) Loading recipe
+    renderSpinner();
     const res = await fetch(
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
     const data = await res.json();
     console.log(data);
@@ -150,4 +156,7 @@ const showRecipe = async function () {
   }
 };
 
-showRecipe();
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
+
+// window.addEventListener('hashchange', showRecipe);
+// window.addEventListener('load', showRecipe);
