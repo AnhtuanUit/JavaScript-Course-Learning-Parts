7d3567c4b916b052600493f30246524e9292df39
TuanDo <anhtuan7194@gmail.com>
2024-01-20 21:03:11 +0700

    Rendering the recipe

--- 18-forkify/src/js/controller.js
+++ 18-forkify/src/js/controller.js
@@ -1,3 +1,7 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+import icons from 'url:../img/icons.svg';
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -11,12 +15,110 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const renderHtml = function (recipe) {
+  const html = `<figure class="recipe__fig">
+      <img src="${recipe.imageUrl}" alt="Tomato" class="recipe__img" />
+      <h1 class="recipe__title">
+        <span>${recipe.title}</span>
+      </h1>
+    </figure>
+
+    <div class="recipe__details">
+      <div class="recipe__info">
+        <svg class="recipe__info-icon">
+          <use href="${icons}.svg#icon-clock"></use>
+        </svg>
+        <span class="recipe__info-data recipe__info-data--minutes">${
+          recipe.cookingTime
+        }</span>
+        <span class="recipe__info-text">minutes</span>
+      </div>
+      <div class="recipe__info">
+        <svg class="recipe__info-icon">
+          <use href="${icons}.svg#icon-users"></use>
+        </svg>
+        <span class="recipe__info-data recipe__info-data--people">${
+          recipe.servings
+        }</span>
+        <span class="recipe__info-text">servings</span>
+
+        <div class="recipe__info-buttons">
+          <button class="btn--tiny btn--increase-servings">
+            <svg>
+              <use href="${icons}.svg#icon-minus-circle"></use>
+            </svg>
+          </button>
+          <button class="btn--tiny btn--increase-servings">
+            <svg>
+              <use href="${icons}.svg#icon-plus-circle"></use>
+            </svg>
+          </button>
+        </div>
+      </div>
+
+      <div class="recipe__user-generated">
+        <svg>
+          <use href="${icons}.svg#icon-user"></use>
+        </svg>
+      </div>
+      <button class="btn--round">
+        <svg class="">
+          <use href="${icons}.svg#icon-bookmark-fill"></use>
+        </svg>
+      </button>
+    </div>
+
+    <div class="recipe__ingredients">
+      <h2 class="heading--2">Recipe ingredients</h2>
+      <ul class="recipe__ingredient-list">
+       ${recipe.ingredients
+         .map(
+           ingredient => `<li class="recipe__ingredient">
+        <svg class="recipe__icon">
+          <use href="${icons}.svg#icon-check"></use>
+        </svg>
+        <div class="recipe__quantity">${ingredient.quantity ?? 1}</div>
+        <div class="recipe__description">
+          <span class="recipe__unit">${ingredient.unit}</span>
+          ${ingredient.description}
+        </div>
+      </li>`
+         )
+         .join('')}
+
+      </ul>
+    </div>
+
+    <div class="recipe__directions">
+      <h2 class="heading--2">How to cook it</h2>
+      <p class="recipe__directions-text">
+        This recipe was carefully designed and tested by
+        <span class="recipe__publisher">${
+          recipe.publisher
+        }</span>. Please check out
+        directions at their website.
+      </p>
+      <a
+        class="btn--small recipe__btn"
+        href="${recipe.sourceUrl}"
+        target="_blank"
+      >
+        <span>Directions</span>
+        <svg class="search__icon">
+          <use href="${icons}.svg#icon-arrow-right"></use>
+        </svg>
+      </a>
+    </div>`;
+  recipeContainer.innerHTML = '';
+  recipeContainer.insertAdjacentHTML('beforeend', html);
+};
+
 const showRecipe = async function () {
   try {
     const res = await fetch(
       'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
     );
-    console.log(res);
     const data = await res.json();
     console.log(data);
     let { recipe } = data.data;
@@ -31,6 +133,7 @@ const showRecipe = async function () {
       id: recipe.id,
     };
     console.log(recipe);
+    renderHtml(recipe);
   } catch (err) {
     alert(err);
   }
